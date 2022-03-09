87e160480493e36d22939ee92dd566239b48f0ae
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-09 18:30:17 +0100

    esercizio-senza-bonus

--- js/main.js
+++ js/main.js
@@ -34,5 +34,10 @@ const team = [
 
 for(let key in team){
     console.log(team[key]);
-} 
+}  
+/* console.log(team) */
+console.log(team[1].name)
 
+document.getElementById("descrizione").innerHTML = (team[0].name + team[0].role);
+document.getElementById("descrizione2").innerHTML= (team[1].name + team[1].role);
+document.getElementById("descrizione3").innerHTML= (team[2].name + team[2].role);
